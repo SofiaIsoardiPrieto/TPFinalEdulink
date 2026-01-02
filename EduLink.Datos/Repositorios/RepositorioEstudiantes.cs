e8ca6949b764d1f03ac9cb83c8fa37048f53d573
Sofía <sofia1chofi@gmail.com>
2026-01-02 16:25:41 -0300

    Uso de procedimientos en repo Estudiantes

--- EduLink.Datos/Repositorios/RepositorioEstudiantes.cs
+++ EduLink.Datos/Repositorios/RepositorioEstudiantes.cs
@@ -5,6 +5,7 @@ using EduLink.Entidades.Dtos;
 using EduLink.Entidades.Entidades;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 
@@ -17,6 +18,7 @@ namespace EduLink.Datos.Repositorios
         {
             cadenaConexion = ConfigurationManager.ConnectionStrings["MiConexion"].ToString();
         }
+
         /// <summary>
         /// Agrega un estudiante a la base de datos
         /// </summary>
@@ -25,12 +27,16 @@ namespace EduLink.Datos.Repositorios
         {
             using (var conn = new SqlConnection(cadenaConexion))
             {
-                string insertQuery = @"INSERT INTO Estudiantes (Legajo, Nombres, Apellidos, Direccion, Telefono, DNI, Email, Contrasenia, FechaNacimiento) 
-                        VALUES(@Legajo, @Nombres, @Apellidos, @Direccion, @Telefono, @DNI, @Email, @Contrasenia, @FechaNacimiento); SELECT SCOPE_IDENTITY()";
-                int id = conn.QuerySingle<int>(insertQuery, estudiante);
+                int id = conn.QuerySingle<int>(
+                    "sp_InsertEstudiante",
+                    estudiante,
+                    commandType: CommandType.StoredProcedure
+                );
+
                 estudiante.EstudianteId = id;
             }
         }
+
         /// <summary>
         /// Determina si ya existe un estudiante en la base de datos
         /// </summary>
@@ -38,31 +44,18 @@ namespace EduLink.Datos.Repositorios
         /// <returns></returns>
         public bool Existe(Estudiante estudiante)
         {
-            var cantidad = 0;
             using (var conn = new SqlConnection(cadenaConexion))
             {
-                string selectQuery;
-                if (estudiante.EstudianteId == 0)
-                {
-                    //cuando es nuevo el estudiante y hay que agregar, ver si ya existe.
+                int cantidad = conn.ExecuteScalar<int>(
+                    "sp_ExisteEstudiante",
+                    estudiante,
+                    commandType: CommandType.StoredProcedure
+                );
 
-                    selectQuery = @"SELECT COUNT(*) FROM Estudiantes 
-                            WHERE Nombres=@Nombres AND Apellidos=@Apellidos";
-                    cantidad = conn.ExecuteScalar<int>(selectQuery, estudiante);
-                }
-                else
-                {
-                    //cuando es una edicion y veo si ya existe el estudiante.
-
-                    selectQuery = @"SELECT COUNT(*) FROM Estudiantes 
-                           WHERE Nombres=@Nombres AND Apellidos=@Apellidos
-                            AND EstudianteId!=@EstudianteId";
-                    cantidad = conn.ExecuteScalar<int>(selectQuery, estudiante);
-                }
+                return cantidad > 0;
             }
-            return cantidad > 0;
         }
-       
+
         /// <summary>
         /// Edita un estudiante en la base de datos
         /// </summary>
@@ -71,14 +64,14 @@ namespace EduLink.Datos.Repositorios
         {
             using (var conn = new SqlConnection(cadenaConexion))
             {
-
-                string updateQuery = @"UPDATE Estudiantes SET Legajo=@Legajo, Nombres=@Nombres,
-                                Apellidos=@Apellidos, Direccion=@Direccion, Telefono=@Telefono, 
-                                DNI=@DNI,Email=@Email, Contrasenia=@Contrasenia, FechaNacimiento=@FechaNacimiento                                 
-                                WHERE EstudianteId=@EstudianteId";
-                conn.Execute(updateQuery, estudiante);
+                conn.Execute(
+                    "sp_UpdateEstudiante",
+                    estudiante,
+                    commandType: CommandType.StoredProcedure
+                );
             }
         }
+
         /// <summary>
         /// Determina si hay un estudiante relacionado con alguna otra tabla
         /// </summary>
@@ -86,14 +79,18 @@ namespace EduLink.Datos.Repositorios
         /// <returns></returns>
         public bool EstaRelacionado(int estudianteId)
         {
-            int cantidad = 0;
-            string selectQuery = @"SELECT COUNT(*) FROM Estudiantes WHERE EstudianteId=@estudianteId";
             using (var conn = new SqlConnection(cadenaConexion))
             {
-                cantidad = conn.ExecuteScalar<int>(selectQuery);
+                int cantidad = conn.ExecuteScalar<int>(
+                    "sp_EstudianteRelacionado",
+                    new { EstudianteId = estudianteId },
+                    commandType: CommandType.StoredProcedure
+                );
+
+                return cantidad > 0;
             }
-            return cantidad > 0;
         }
+
         /// <summary>
         /// Borra un estudiante de la base de datos
         /// </summary>
@@ -102,10 +99,14 @@ namespace EduLink.Datos.Repositorios
         {
             using (var conn = new SqlConnection(cadenaConexion))
             {
-                string deleteQuery = "DELETE FROM Estudiantes WHERE EstudianteId=@EstudianteId";
-                conn.Execute(deleteQuery, new { estudianteId });
+                conn.Execute(
+                    "sp_DeleteEstudiante",
+                    new { EstudianteId = estudianteId },
+                    commandType: CommandType.StoredProcedure
+                );
             }
         }
+
         /// <summary>
         /// Obtiene la cantidad de registros de estudiantes, si se pasa un textoFiltro, 
         /// filtra por apellido que comienze con ese texto
@@ -114,23 +115,18 @@ namespace EduLink.Datos.Repositorios
         /// <returns></returns>
         public int GetCantidad(string textoFiltro = null)
         {
-            int cantidad = 0;
             using (var conn = new SqlConnection(cadenaConexion))
             {
-                string selectQuery;
-                if (textoFiltro == null)
-                {
-                    selectQuery = @"SELECT COUNT(*) FROM Estudiantes";
-                    cantidad = conn.ExecuteScalar<int>(selectQuery);
-                }
-                else
-                {
-                    selectQuery = @"SELECT COUNT(*) FROM Estudiantes  WHERE Apellido LIKE @textoFiltro + '%'";
-                    cantidad = conn.ExecuteScalar<int>(selectQuery, new { textoFiltro });
-                }
+                int cantidad = conn.ExecuteScalar<int>(
+                    "sp_GetCantidadEstudiantes",
+                    new { TextoFiltro = textoFiltro },
+                    commandType: CommandType.StoredProcedure
+                );
+
+                return cantidad;
             }
-            return cantidad;
         }
+
         /// <summary>
         /// Devuelve una lista de estudiantes paginada, 
         /// si se pasa un textoFiltro, filtra por apellido que comienze con ese texto
@@ -141,52 +137,35 @@ namespace EduLink.Datos.Repositorios
         /// <returns></returns>
         public List<EstudianteDto> GetEstudiantesPorPagina(int cantidadPorPagina, int paginaActual, string textoFiltro = null)
         {
-            List<EstudianteDto> lista = new List<EstudianteDto>();
             using (var conn = new SqlConnection(cadenaConexion))
             {
-                string selectQuery;
-                if (textoFiltro == null)
-                {
-                    selectQuery = @" SELECT *
-                      FROM vw_EstudiantesConCiudad
-                      ORDER BY Apellidos,Nombres
-                      OFFSET @cantidadRegistros ROWS FETCH NEXT @cantidadPorPagina ROWS ONLY";
-                    var cantidadRegistros = cantidadPorPagina * (paginaActual - 1);
-                    lista = conn.Query<EstudianteDto>(selectQuery, new { cantidadRegistros, cantidadPorPagina }).ToList();
-                }
-                else
-                {
-                    selectQuery = @"SELECT *
-                      FROM vw_EstudiantesConCiudad
-                      WHERE Apellido like @textoFiltro + '%'
-                      ORDER BY Apellido,Nombre
-                      OFFSET @cantidadRegistros ROWS FETCH NEXT @cantidadPorPagina ROWS ONLY";
-                    var cantidadRegistros = cantidadPorPagina * (paginaActual - 1);
-                    lista = conn.Query<EstudianteDto>(selectQuery, new { cantidadRegistros, cantidadPorPagina, textoFiltro }).ToList();
-                }
+                var lista = conn.Query<EstudianteDto>(
+                    "sp_GetEstudiantesPorPagina",
+                    new { CantidadPorPagina = cantidadPorPagina, PaginaActual = paginaActual, TextoFiltro = textoFiltro },
+                    commandType: CommandType.StoredProcedure
+                ).ToList();
+
+                return lista;
             }
-            return lista;
         }
-      
+
         /// <summary>
         /// Recive una lista de estudiantes para llenar un combo
         /// </summary>
         /// <returns></returns>
         public List<EstudianteCombo> GetEstudiantesCombo()
         {
-            List<EstudianteCombo> lista = new List<EstudianteCombo>();
-
             using (var conn = new SqlConnection(cadenaConexion))
             {
+                var lista = conn.Query<EstudianteCombo>(
+                    "sp_GetEstudiantesCombo",
+                    commandType: CommandType.StoredProcedure
+                ).ToList();
 
-                string selectQuery = @"SELECT EstudianteId, CONCAT(Apellido,', ',Nombre) as NombreCompleto
-                            FROM Estudiantes
-                            ORDER BY NombreCompleto";
-                lista = conn.Query<EstudianteCombo>(selectQuery).ToList();
+                return lista;
             }
-            return lista;
         }
- 
+
     }
 }
 
